dbe70c244fc55867d0c40b1ca4847a5d20e5a390
Fernando Lara <larafernandoagustin@gmail.com>
2022-09-02 19:31:11 -0300

    Calculadora de baldozas con function y ciclo while

--- main.js
+++ main.js
@@ -1,10 +1,20 @@
-let numero = parseInt(prompt("Ingrese la cantidad de veces que quiera que se repita el mensaje: "));
+alert("¡Bienvenido al calculador de baldozas!")
 
-for(let i = 1; i <= numero; i++) {
-    if(numero >= 10000) {
-        alert("No rompas tu computadora!");
-        numero = parseInt(prompt("Ingresa un numero menor a 10.000"));
-    }else {
-        console.log("Hola mundo", i);
-    }
+let tipoBaldoza = parseInt(prompt("Ingrese la longitud del lado de su baldoza en centímetros (Coloque sólo el nro: 20, 30, 50, etc.) "));
+
+let baldoza = tipoBaldoza * tipoBaldoza;
+
+let largo = parseInt(prompt("Ingrese el largo de su piso en metros (introduza sólo números): "));
+let ancho = parseInt(prompt("Ingrese el ancho de su piso en metros (introduzca sólo números): "));
+
+function cantBaldozas(largo, ancho, baldoza) { 
+    return (largo * ancho * 10000) / baldoza;
 }
+
+while(isNaN(cantBaldozas(largo, ancho, baldoza)) == true){
+    alert("Valores ingresados no válidos, recargue el sitio e introduzca sólo números.");
+    break;
+}
+
+alert("La cantidad de baldozas que necesita para cubrir su piso es de " + cantBaldozas(largo, ancho, baldoza));
+
