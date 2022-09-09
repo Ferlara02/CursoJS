45cdf5ac4b896944459423d2bfd4767a8db2bab0
Fernando Lara <larafernandoagustin@gmail.com>
2022-09-09 20:00:19 -0300

    Agregando objeto y array

--- main.js
+++ main.js
@@ -1,20 +1,43 @@
 alert("¡Bienvenido al calculador de baldozas!")
 
-let tipoBaldoza = parseInt(prompt("Ingrese la longitud del lado de su baldoza en centímetros (Coloque sólo el nro: 20, 30, 50, etc.) "));
+//Objeto Tipo baldoza y piso
+class calculador {
+    constructor(nombre, lado1, lado2, largo, ancho) {
+        this.nombre = nombre;
+        this.lado1 = lado1;
+        this.lado2 = lado2;
+        this.largo = largo;
+        this.ancho = ancho;
+    }
+    calcBaldozas() { 
+        return ((this.largo * this.ancho * 10000) / (this.lado1 * this.lado2));
+    }
+}
+const arrayCalculos = [];
 
-let baldoza = tipoBaldoza * tipoBaldoza;
+function menu() {
+    let nombre = prompt("Ingrese el nombre del ambiente (ej: Living): ");
+    let lado1 = parseInt(prompt("Ingrese un lado de la baldoza en centímetros: "));
+    let lado2 = parseInt(prompt("Ingrese el otro lado de la baldoza en centímetros: "));
+    let largo = parseInt(prompt("Ingrese el largo del piso en metros: "));
+    let ancho = parseInt(prompt("Ingrese el ancho del piso en metros: "));
 
-let largo = parseInt(prompt("Ingrese el largo de su piso en metros (introduza sólo números): "));
-let ancho = parseInt(prompt("Ingrese el ancho de su piso en metros (introduzca sólo números): "));
+    baldozaPersonalizada = new calculador(nombre, lado1, lado2, largo, ancho);
+    console.log(baldozaPersonalizada);
 
-function cantBaldozas(largo, ancho, baldoza) { 
-    return (largo * ancho * 10000) / baldoza;
+    alert("La cantidad de baldozas que necesita para cubrir su " + nombre + " es de: " + baldozaPersonalizada.calcBaldozas());
+
+    arrayCalculos.push(baldozaPersonalizada.calcBaldozas());
 }
 
-while(isNaN(cantBaldozas(largo, ancho, baldoza)) == true){
-    alert("Valores ingresados no válidos, recargue el sitio e introduzca sólo números.");
-    break;
+let opcion = parseInt(prompt("Ingrese la cantidad de pisos que desea calcular: (Por ejemplo, en caso de que quiera calcular el living y comedor con diferentes baldozas cada uno, ingrese 2.) " ));
+
+let sumaBaldozas = 0;
+
+for (let i = 0; i < opcion; i++){
+    menu()
+    console.log(arrayCalculos);
+    sumaBaldozas += arrayCalculos[i];
 }
 
-alert("La cantidad de baldozas que necesita para cubrir su piso es de " + cantBaldozas(largo, ancho, baldoza));
-
+alert("La cantidad total de baldozas para los " + opcion + " pisos es de: " + sumaBaldozas);
