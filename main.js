ee28b8ac577fed77bcf93bb58a8bbb79f28c2079
Fernando Lara <larafernandoagustin@gmail.com>
2022-09-15 17:22:49 -0300

    Añadida función buscar con .find y utlilizacion de .reduce

--- main.js
+++ main.js
@@ -1,43 +1,56 @@
 alert("¡Bienvenido al calculador de baldozas!")
 
-//Objeto Tipo baldoza y piso
+//Objeto: Tipo baldoza y piso
 class pisoYBaldoza {
-    constructor(nombre, lado1, lado2, largo, ancho) {
+    constructor(nombre, lado1, lado2, largo, ancho, cantidad) {
         this.nombre = nombre;
         this.lado1 = lado1;
         this.lado2 = lado2;
         this.largo = largo;
         this.ancho = ancho;
-    }
-    calcBaldozas() { 
-        return ((this.largo * this.ancho * 10000) / (this.lado1 * this.lado2));
+        this.cantidad = cantidad;
     }
 }
-const arrayCalculos = [];
+//Array
+const arrayBaldozas = [];
 
+//Funciones
 function calculadoraCantidad() {
-    let nombre = prompt("Ingrese el nombre del ambiente (ej: Living): ");
+    let nombre = (prompt("Ingrese el nombre del ambiente (ej: Living): ")).toLowerCase();
     let lado1 = parseInt(prompt("Ingrese un lado de la baldoza en centímetros: "));
     let lado2 = parseInt(prompt("Ingrese el otro lado de la baldoza en centímetros: "));
     let largo = parseInt(prompt("Ingrese el largo del piso en metros: "));
     let ancho = parseInt(prompt("Ingrese el ancho del piso en metros: "));
+    let cantidad = (largo * ancho * 10000) / (lado1 * lado2);
 
-    baldozaPersonalizada = new pisoYBaldoza(nombre, lado1, lado2, largo, ancho);
+    baldozaPersonalizada = new pisoYBaldoza(nombre, lado1, lado2, largo, ancho, cantidad);
     console.log(baldozaPersonalizada);
 
-    alert("La cantidad de baldozas que necesita para cubrir su " + nombre + " es de: " + baldozaPersonalizada.calcBaldozas());
+    alert("La cantidad de baldozas que necesita para cubrir su " + nombre + " es de: " + baldozaPersonalizada.cantidad);
 
-    arrayCalculos.push(baldozaPersonalizada.calcBaldozas());
+    arrayBaldozas.push(baldozaPersonalizada);
 }
 
 let opcion = parseInt(prompt("Ingrese la cantidad de pisos que desea calcular: (Por ejemplo, en caso de que quiera calcular el living y comedor con diferentes baldozas cada uno, ingrese 2.) " ));
 
-let sumaBaldozas = 0;
+let totalBaldozas;
 
 for (let i = 0; i < opcion; i++){
     calculadoraCantidad()
-    console.log(arrayCalculos);
-    sumaBaldozas += arrayCalculos[i];
+    //sumaBaldozas += arrayCalculos[i];
+    totalBaldozas = arrayBaldozas.reduce((acumulador, elemento) => acumulador + elemento.cantidad, 0);
 }
 
-alert("La cantidad total de baldozas para los " + opcion + " pisos es de: " + sumaBaldozas);
+if (opcion != 1){
+    alert("La cantidad total de baldozas para los " + opcion + " pisos es de: " + totalBaldozas);
+}else alert("La cantidad total de baldozas para: " + baldozaPersonalizada.nombre + " es de: " + totalBaldozas);
+
+//Funcion buscar 
+let deseaBuscar = (prompt("Desea buscar alguno de los cálculos realizados? Ingrese si o no: ")).toLowerCase();
+
+if (deseaBuscar == "si"){
+    let nombreIngresado = (prompt("Ingrese el nombre del ambiente calculado: ")).toLowerCase();
+
+    const buscado = arrayBaldozas.find(pisoYBaldoza => pisoYBaldoza.nombre === nombreIngresado);
+    alert("La cantidad de baldozas para " + buscado.nombre + " es de " + buscado.cantidad);
+}
